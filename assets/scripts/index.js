4fde2fe42c6eb3b2f305b997f9fdf2a85be4c71f
hadleyana <hadleysweck@gmail.com>
2017-10-14 12:39:47 -0400

    Updated Styling
    color declarations
    spacing
    icons
    contact page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,6 +5,30 @@ const config = require('./config')
 
 $(() => {
   setAPIOrigin(location, config)
+  $('#aboutTab').on('click', function () {
+    $('#about').removeClass(' hidden')
+    $('#projects').addClass(' hidden')
+    $('#skills').addClass(' hidden')
+    $('#contact').addClass(' hidden')
+  })
+  $('#projectsTab').on('click', function () {
+    $('#about').addClass(' hidden')
+    $('#projects').removeClass(' hidden')
+    $('#skills').addClass(' hidden')
+    $('#contact').addClass(' hidden')
+  })
+  $('#skillsTab').on('click', function () {
+    $('#about').addClass(' hidden')
+    $('#projects').addClass(' hidden')
+    $('#skills').removeClass(' hidden')
+    $('#contact').addClass(' hidden')
+  })
+  $('#contactTab').on('click', function () {
+    $('#about').addClass(' hidden')
+    $('#projects').addClass(' hidden')
+    $('#skills').addClass(' hidden')
+    $('#contact').removeClass(' hidden')
+  })
 })
 
 // use require with a reference to bundle the file and use it in this file
